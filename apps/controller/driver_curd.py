872066f81bd74bbe7d8d3ad0f97e2930ba196971
EDZ <wangyk@tico.cn>
2020-09-26 17:47:05 +0800

    修改driver_curd.py，使用db文件夹下db对象

--- apps/controller/driver_curd.py
+++ apps/controller/driver_curd.py
@@ -9,42 +9,28 @@ import datetime
 
 import pymysql
 from fastapi import *
-from apps.controller import router
 from fastapi.responses import JSONResponse
 
-
-# mysql 连接
-def mysql_client():
-    db = pymysql.connect(
-        host='127.0.0.1',
-        port=3306,
-        user='root',
-        password='123456',
-        database='driver_manager',
-        charset='utf8'
-    )
-    return db
+from apps.controller import router
+from apps.db.db import db
 
 
 @router.get('/getInfos/', response_class=JSONResponse, tags=['select'])
 async def get_infos():
-    db = mysql_client()
     sql = 'select * from t_drivers;'
-    cursor = db.cursor()
+    cursor = db.cursor
     cursor.execute(sql)
     results = cursor.fetchall()
     driver_infos = []
 
-    for result in results:
-        one_data = list(result)
-        temp = []
-        for i in one_data:
-            if isinstance(i, datetime.datetime):
-                i = i.strftime("%Y-%m-%d %H:%M:%S")
-                temp.append(i)
-            else:
-                temp.append(i)
-        driver_infos.append(dict(zip([key[0] for key in cursor.description], temp)))
+    temp = []
+    for i in results:
+        if isinstance(i, datetime.datetime):
+            i = i.strftime("%Y-%m-%d %H:%M:%S")
+            temp.append(i)
+        else:
+            temp.append(i)
+    driver_infos.append(dict(zip([key[0] for key in cursor.description], temp)))
 
     return {'driver_all': driver_infos}
 

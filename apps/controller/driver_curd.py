be0dfd06144a3a9c88bebbe56f318fc1e15fd205
EDZ <wangyk@tico.cn>
2020-10-29 17:57:26 +0800

    修改driver_curd.py，查询逻辑修改

--- apps/controller/driver_curd.py
+++ apps/controller/driver_curd.py
@@ -6,6 +6,7 @@
 t_driver表增删改查接口
 """
 import datetime
+import json
 
 import pymysql
 from fastapi import *
@@ -17,12 +18,17 @@ from apps.db.db import db
 
 @router.get('/getInfos/', response_class=JSONResponse, tags=['select'])
 async def get_infos():
-    sql = 'select * from t_drivers;'
-    results = db.select(sql)
+    select_all = 'select * from t_drivers;'
+    results = db.select(select_all)
 
     return {'driver_all': results}
 
 
 @router.post('/creatDriver/', tags=['creat'])
-async def insert_infos(driver_json: str = Form(...)):
+async def insert_infos(driver_json: str):
+    one_driver = json.loads(driver_json)
+    path = one_driver.get('path')
+    kind_category = one_driver.get('kind_category')
+    insert_one = ''
+    db.insert(insert_one)
     return

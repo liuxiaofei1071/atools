076b5f53e46e454d44322e805aa2f6f118aea66b
liuxiaofei <liuxiaofeikeke@163.com>
2020-12-28 20:19:56 +0800

    优化接口2

--- apps/service/file_manager_service.py
+++ apps/service/file_manager_service.py
@@ -28,7 +28,6 @@ async def upload(file):
     # 获取文件名称
     dir_name_dict = content_type_sql.select_content_dir_record(content_type, file_suffix)
     dir_name = dir_name_dict.get("name", "")
-
     # 校验目录是否为系统支持资源目录
     if dir_name in TypePath:
         _directory_name_path = os.path.join(RESOURCE_PATH, dir_name)
@@ -52,6 +51,7 @@ async def upload(file):
             resource_sql.create_file(resource_id, file_name, size, file_suffix, filename_full_path)
 
             response = {}
+            response["id"] = resource_id
             response["path"] = filename_full_path
             response["filename"] = filename
             response["content_type"] = content_type
